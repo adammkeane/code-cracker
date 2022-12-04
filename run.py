0fe2e5bd073a99bacc8132ba225e226994b1fea4
adammkeane <adamkeane.edu@gmail.com>
2022-12-04 22:53:41 +0000

    Initial commit and import pyfiglet

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+# python3 run.py
+
+# import pyfiglet
+from pyfiglet import Figlet
+
+logo = Figlet(font="5lineoblique")
+print(logo.renderText("Code Cracker"))
