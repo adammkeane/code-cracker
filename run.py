9a15680512a5ff48cffb40e769c91eeddae0601a
adammkeane <adamkeane.edu@gmail.com>
2022-12-06 21:58:08 +0000

    Add intro choice function and main function

--- run.py
+++ run.py
@@ -6,7 +6,31 @@
 # import pyfiglet
 from pyfiglet import Figlet
 
+
+def get_intro_choice():
+    """
+    Get intro selection from user: whether they want to play the game, or see how to play first
+    """
+    while True:
+        print("1 : Play Game")
+        print("2 : How to Play\n")
+        print("Type '1' or '2' for the option you want to select, and then press Enter please.")
+
+        intro_choice = input("Enter your selection here: ")
+        
+        
+        if validate_data(intro_choice):
+            print("What a fine choice.")
+            break
+
+    return intro_choice       
+
+def main():
+    get_intro_choice()
+
 logo = Figlet(font="5lineoblique")
 print(logo.renderText("Code Cracker"))
 print("Crack the code and win 1 million dollars.")
-print("(actual cash prize may be sigificantly less than that)")
+print("(actual cash prize may be sigificantly less than that)\n")
+
+main()
